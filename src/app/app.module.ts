814ff28dc53700ac536222f1c383b00a5ac4e131
Algorithme123 <kossi.gogo@ipnetinstitute.com>
2022-12-21 18:47:50 +0000

    fonctionnel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PdfGenerateurComponent } from './pdf-generateur/pdf-generateur.component';
+import { BadgeComponent } from './badge/badge.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PdfGenerateurComponent
+    PdfGenerateurComponent,
+    BadgeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/badge/badge.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import jsPDF from 'jspdf';
+import html2canvas from 'html2canvas';
+
+@Component({
+  selector: 'app-badge',
+  templateUrl: './badge.component.html',
+  styleUrls: ['./badge.component.css']
+})
+export class BadgeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  USERS = [
+    {
+      "id": 1,
+      "name": "Leanne Graham",
+      "email": "sincere@april.biz",
+      "phone": "1-770-736-8031 x56442"
+    },
+
+  ];
+
+  openPDF(): void {
+    let DATA: any = document.getElementById('htmlData');
+    html2canvas(DATA).then((canvas) => {
+        let fileWidth = 200
+        let fileHeight =(canvas.height * fileWidth)/canvas.width;
+        const FILEURI =canvas.toDataURL('image/png');
+        let PDF = new jsPDF('p', 'mm', 'a4');
+        let position = 0;
+        PDF.addImage(FILEURI, 'PNG', 0, position, fileWidth, fileHeight);
+        PDF.save('angular-demo.pdf');
+        }
+        );
+    }
+
+
+
+}
